ad6c4ea0e03e5ccf64683bff98b5acca409ce14f
ehdgn <ehdgn@jiwon>
2025-04-06 17:25:52 +0900

    init

---
+++ src/test/java/com/frogzon/email/EmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frogzon.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
